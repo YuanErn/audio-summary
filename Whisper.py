062d4a8f85b8eed4855815c79bdffcd51582f987
YuanErn <yuanernc@gmail.com>
2023-03-24 23:35:22 +0800

    Git Ignore Update

--- Whisper.py
+++ Whisper.py
@@ -15,10 +15,10 @@ mel = whisper.log_mel_spectrogram(whisperAudio).to(whisperModel.device)
 
 whisperResult = str(whisperModel.transcribe(whisperAudio, language = "Chinese")) # Specify language when possible
 
-# Context for the data above
+# Context for the data. This will be passed on to ChatGPT
 meetingTopic = "a meeting session for trial balance dashboard review session" # What was the meeting about?
 partiesInvolved = "developer and client" # Could be an internal meeting or one with clients
-actionToDo = "summarize this meeting into meeting minutes" # What do u want to be done with this information
+actionToDo = "summarize this meeting into meeting minutes. Point form" # What do u want to be done with this information
 prompt = "This is " + meetingTopic + " between " + partiesInvolved + "." + actionToDo
 
 # Settings for the model
